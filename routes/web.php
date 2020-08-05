c101e0a7f6aa375a3727272b29be7f26df719a5f
geofany <172410102040@cs.unej.ac.id>
2020-08-06 00:56:06 +0700

    create UI SSL Checker Tools

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::get('/{lang}/symbol-and-text-generator', 'ToolsController@symbolandtext')
 Route::get('/{lang}/page-speed', 'ToolsController@pagespeed');
 Route::get('/{lang}/sitemap-generator', 'ToolsController@sitemap');
 Route::get('/{lang}/mobile-test', 'ToolsController@mobiletest');
+Route::get('/{lang}/ssl-checker', 'ToolsController@sslchecker');
 Route::get('/construction', function (){
     return view('premiumtools');
 });
