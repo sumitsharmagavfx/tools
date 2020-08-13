743c6052a03b72d0da43e092d0463809a4f6bb91
Ermanu Azizul Hakim <42911371+ermanuk@users.noreply.github.com>
2020-08-13 11:24:50 +0700

    fix html, alternate lang

--- resources/lang/id/home.php
+++ resources/lang/id/home.php
@@ -3,5 +3,6 @@ $data = count(json_decode(file_get_contents(base_path('resources/js/json/tools.j
 return [
     "meta-title" => $data." SEO Tools (Gratis) Versi Bahasa Indonesia \ cmlabs",
     "meta-desc" => "Cek website dengan Free SEO Tools cmlabs: Page Speed Test, SItemap Generator, Word Counter, Title & Meta Desc Checker, dll.",
-    "meta-keyword" => "seo, free seo tool, content writing, copywriting"
+    "meta-keyword" => "seo, free seo tool, content writing, copywriting",
+    "lang" => "id"
 ];
