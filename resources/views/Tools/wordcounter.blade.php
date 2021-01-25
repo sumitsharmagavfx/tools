c17ea40b14f38c4b0a963c813c399711a0af697d
geofany <172410102040@cs.unej.ac.id>
2021-01-25 12:41:05 +0700

    test push

--- resources/views/Tools/wordcounter.blade.php
+++ resources/views/Tools/wordcounter.blade.php
@@ -693,7 +693,7 @@ id/word-counter
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
       <div class="col-md-2">
-        <p>SHALOM ALAIKOM</p>
+        <p>Why does seo important</p>
       </div>
     </div>
     <div class="my-10" style="background:var(--darkgrey); border-radius:20px">
