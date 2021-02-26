74736a01de53be8e3b006d7442731aa7b6c2bdc4
whykresna <44244163+whykresna@users.noreply.github.com>
2021-02-26 09:48:42 +0700

    jsonld recipe frontend revamp + fix minor styling problem

--- public/js/logic/recipe-json.js
+++ public/js/logic/recipe-json.js
@@ -54,8 +54,11 @@ jQuery(document).ready(function () {
         }
         print();
         
-        jQuery('#image').append("<button type=\"button\" class=\"btn btn-danger mb-2 deleteImage\" name=\"button\" data-id=\""+(main.image.length-1)+"\">"+deletes+"</button>\n" +
-            "                <input type=\"text\" name=\"\" class=\"form-control mb-5 image\" placeholder=\""+imageUrl+" :\" value=\"\" data-id=\""+(main.image.length-1)+"\">"
+        // jQuery('#image').append("<button type=\"button\" class=\"btn btn-danger mb-2 deleteImage\" name=\"button\" data-id=\""+(main.image.length-1)+"\">"+deletes+"</button>\n" +
+        //     "                <input type=\"text\" name=\"\" class=\"form-control mb-5 image\" placeholder=\""+imageUrl+" :\" value=\"\" data-id=\""+(main.image.length-1)+"\">"
+        // );
+        jQuery('#image').append("<div class=\"row\" data-id=\""+(main.image.length-1)+"\"><div class=\"col-10 col-sm-11\"><label class=\"text-black font-weight-bold\" for=\"image\" data-id=\""+(main.image.length-1)+"\">Image URL #"+(main.image.length)+"</label>\n" +
+        "                <input type=\"text\" name=\"\" class=\"form-control image mb-5\" placeholder=\"Type image url here..\" value=\"\" data-id=\""+(main.image.length-1)+"\"></div><div class=\"col-2 col-sm-1\"><div class=\"d-flex justify-content-center mt-9\"><i class=\'bx bxs-x-circle bx-md text-darkgrey deleteImage\' data-id=\""+(main.image.length-1)+"\"></i></div></div></div>\n"
         );
         let row = parseInt(jQuery('#json-format').val().split('\n').length);
         jQuery('#json-format').attr('rows',row);
@@ -65,31 +68,38 @@ jQuery(document).ready(function () {
 });
 
 jQuery(document).on('click', '#add-ingredients', function () {
-    let index = parseInt(jQuery(this).data('id'));
-    let ingredients = lang==='en'?'Ingredients':'Bahan';
-    let deletes = lang ==='en'? 'Delete' : 'Hapus';
+    // let index = parseInt(jQuery(this).data('id'));
+    // let ingredients = lang==='en'?'Ingredients':'Bahan';
+    // let deletes = lang ==='en'? 'Delete' : 'Hapus';
 
-    main.mainEntity.ingredients=[""];
-    jQuery('#ingredients').append("<button type=\"button\" class=\"btn btn-danger mb-2 deleteIngredients\" name=\"button\" data-id=\""+(main.mainEntity.length-1)+"\">"+deletes+"</button>\n" +
-            "                <input type=\"text\" name=\"\" class=\"form-control mb-5 ingredients\" placeholder=\""+ingredients+" :\" value=\"\" data-id=\""+(main.mainEntity.length-1)+"\">"
-        );
-    let row = parseInt(jQuery('#json-format').val().split('\n').length);
-    jQuery('#json-format').attr('rows',row);
-    sticky.update();
-    print();
+    // main.mainEntity.ingredients=[""];
+    jQuery('#ingredients').append("<div class=\"row\" data-id=\""+(0)+"\"><div class=\"col-10 col-sm-11\"><label class=\"text-black font-weight-bold\" for=\"ingredients\" data-id=\""+(0)+"\">Ingredients #"+(0)+"</label>\n" +
+    "                <input type=\"text\" name=\"\" class=\"form-control ingredients mb-5\" placeholder=\"Type image url here..\" value=\"\" data-id=\""+(0)+"\"></div><div class=\"col-2 col-sm-1\"><div class=\"d-flex justify-content-center mt-9\"><i class=\'bx bxs-x-circle bx-md text-darkgrey deleteIngredients\' data-id=\""+(0)+"\"></i></div></div></div>\n"
+    );
+    // let row = parseInt(jQuery('#json-format').val().split('\n').length);
+    // jQuery('#json-format').attr('rows',row);
+    // sticky.update();
+    // print();
 });
 
-jQuery(document).on('click', '.stepField', function () {
+jQuery(document).on('click', '#add-recipe-step', function () {
     let index = parseInt(jQuery(this).data('id'));
     let step = lang==='en'?'Step':'Langkah Langkah';
     let deletes = lang ==='en'? 'Delete Step' : 'Hapus Langkah';
 
-    jQuery('#step').append("<button type=\"button\" class=\"btn btn-danger mb-2 deleteStep\" name=\"button\" data-id=\""+(main.recipeInstructions.length-1)+"\">"+deletes+"</button>\n" +
-    "                <input type=\"text\" name=\"\" class=\"form-control mb-5 step\" placeholder=\""+step+" :\" value=\"\" data-id=\""+(main.recipeInstructions.length-1)+"\">" +
-    "                <input type=\"text\" name=\"\" class=\"form-control mb-5 nameStep\" placeholder=\" Name Step :\" value=\"\" data-id=\"\">"+
-    "                <input type=\"text\" name=\"\" class=\"form-control mb-5 url\" placeholder=\" URL :\" value=\"\" data-id=\"\">"+
-    "                <input type=\"text\" name=\"\" class=\"form-control mb-5 imageStep\" placeholder=\" Image \" value=\"\" data-id=\"\">"
+    // jQuery('#step').append("<button type=\"button\" class=\"btn btn-danger mb-2 deleteStep\" name=\"button\" data-id=\""+(main.recipeInstructions.length-1)+"\">"+deletes+"</button>\n" +
+    // "                <input type=\"text\" name=\"\" class=\"form-control mb-5 step\" placeholder=\""+step+" :\" value=\"\" data-id=\""+(main.recipeInstructions.length-1)+"\">" +
+    // "                <input type=\"text\" name=\"\" class=\"form-control mb-5 nameStep\" placeholder=\" Name Step :\" value=\"\" data-id=\"\">"+
+    // "                <input type=\"text\" name=\"\" class=\"form-control mb-5 url\" placeholder=\" URL :\" value=\"\" data-id=\"\">"+
+    // "                <input type=\"text\" name=\"\" class=\"form-control mb-5 imageStep\" placeholder=\" Image \" value=\"\" data-id=\"\">"
 
+
+    // );
+    jQuery('#step').append("<div class=\"row\" data-id=\""+(main.recipeInstructions.length-1)+"\"><div class=\"col-6 col-md-3\"><label class=\"text-black font-weight-bold\" for=\"step\" data-id=\""+(main.recipeInstructions.length-1)+"\">Step #"+(main.recipeInstructions.length)+": Text</label><input type=\"text\" name=\"\" class=\"form-control step mb-5\" placeholder=\"Type step here..\" value=\"\" data-id=\""+(main.recipeInstructions.length-1)+"\"></div>" + 
+    "                <div class=\"col-6 col-md-2\"><label class=\"text-black font-weight-bold\" for=\"nameStep\" data-id=\""+(main.recipeInstructions.length-1)+"\">Name</label><input type=\"text\" name=\"\" class=\"form-control nameStep mb-5\" placeholder=\"Type name here..\" value=\"\" data-id=\""+(main.recipeInstructions.length-1)+"\"></div>" + 
+    "                <div class=\"col-6 col-md-3\"><label class=\"text-black font-weight-bold\" for=\"url\" data-id=\""+(main.recipeInstructions.length-1)+"\">URL</label><input type=\"text\" name=\"\" class=\"form-control url mb-5\" placeholder=\"Type name here..\" value=\"\" data-id=\""+(main.recipeInstructions.length-1)+"\"></div>" + 
+    "                <div class=\"col-5 col-md-3\"><label class=\"text-black font-weight-bold\" for=\"imageStep\" data-id=\""+(main.recipeInstructions.length-1)+"\">Image URL</label><input type=\"text\" name=\"\" class=\"form-control imageStep mb-5\" placeholder=\"Type name here..\" value=\"\" data-id=\""+(main.recipeInstructions.length-1)+"\"></div>" + 
+    "                <div class=\"col-1\"><div class=\"d-flex justify-content-center mt-9\"><i class=\'bx bxs-x-circle bx-md text-darkgrey deleteStep\' data-id=\""+(main.recipeInstructions.length-1)+"\"></i></div></div>"
 
     );
     let row = parseInt(jQuery('#json-format').val().split('\n').length);
@@ -319,17 +329,27 @@ jQuery(document).on('change', '.worstRating', function () {
 
 
 
-jQuery(document).on('click', '.addReview', function () {
+jQuery(document).on('click', '#add-recipe-review', function () {
     let index = parseInt(jQuery(this).data('id'));
     let deletes = lang ==='en'? 'Delete Review' : 'Hapus Review';
 
-    jQuery('#addReview').append("<button type=\"button\" class=\"btn btn-danger mb-2 deleteStep\" name=\"button\" data-id=\""+(main.length-1)+"\">"+deletes+"</button>\n" +
-    "                <input type=\"text\" name=\"\" class=\"form-control mb-5 review\" placeholder=\"Review :\" value=\"\" data-id=\""+(main.recipeInstructions.length-1)+"\">" +
-    "                <input type=\"text\" name=\"\" class=\"form-control mb-5 reviewBody\" placeholder=\" reviewBody :\" value=\"\" data-id=\"\">"+
-    "                <input type=\"number\" name=\"\" class=\"form-control mb-5 rating\" placeholder=\" Rating :\" value=\"\" data-id=\"\">"+
-    "                <input type=\"date\" name=\"\" class=\"form-control mb-5 dateReview\" placeholder=\" Date Review \" value=\"\" data-id=\"\">" +
-    "                <input type=\"text\" name=\"\" class=\"form-control mb-5 authorReview\" placeholder=\" Author \" value=\"\" data-id=\"\">"+
-    "                <input type=\"text\" name=\"\" class=\"form-control mb-5 publisher\" placeholder=\" publisher \" value=\"\" data-id=\"\">"
+    // jQuery('#addReview').append("<button type=\"button\" class=\"btn btn-danger mb-2 deleteStep\" name=\"button\" data-id=\""+(main.length-1)+"\">"+deletes+"</button>\n" +
+    // "                <input type=\"text\" name=\"\" class=\"form-control mb-5 review\" placeholder=\"Review :\" value=\"\" data-id=\""+(main.recipeInstructions.length-1)+"\">" +
+    // "                <input type=\"text\" name=\"\" class=\"form-control mb-5 reviewBody\" placeholder=\" reviewBody :\" value=\"\" data-id=\"\">"+
+    // "                <input type=\"number\" name=\"\" class=\"form-control mb-5 rating\" placeholder=\" Rating :\" value=\"\" data-id=\"\">"+
+    // "                <input type=\"date\" name=\"\" class=\"form-control mb-5 dateReview\" placeholder=\" Date Review \" value=\"\" data-id=\"\">" +
+    // "                <input type=\"text\" name=\"\" class=\"form-control mb-5 authorReview\" placeholder=\" Author \" value=\"\" data-id=\"\">"+
+    // "                <input type=\"text\" name=\"\" class=\"form-control mb-5 publisher\" placeholder=\" publisher \" value=\"\" data-id=\"\">"
+    // );
+
+    jQuery('#addReview').append("<div class=\"row\"><div class=\"col-12 col-lg-6\"><label class=\"text-black font-weight-bold\" for=\"review\">#"+(0)+" Review’s name</label><input type=\"text\" name=\"\" class=\"form-control review mb-5\" placeholder=\"Type review’s name here..\" value=\"\" data-id=\""+(0)+"\">" +
+    "                   <div class=\"row\"><div class=\"col-6 col-md-4\"><label class=\"text-black font-weight-bold\" for=\"rating\">Rating</label><input type=\"number\" name=\"\" class=\"form-control rating mb-5\" placeholder=\"0\" value=\"\" min=\"0\" data-id=\""+(0)+"\"></div>" +
+    "                   <div class=\"col-6 col-md-8\"><label class=\"text-black font-weight-bold\" for=\"dateReview\">Date</label><div class=\"input-group date\"><div class=\"input-group-append\"><span class=\"input-group-text\"><i class=\"bx bx-calendar\"></i></span></div>" +
+    "                   <input type=\"text\" id=\"kt_datepicker_2\" name=\"\" class=\"form-control dateReview\" readonly  placeholder=\"Pick a date\" value=\"\" data-id=\""+(0)+"\"/></div></div></div></div>" +
+    "                   <div class=\"col-12 col-lg-6 mb-8 mb-lg-5\"><label class=\"text-black font-weight-bold\" for=\"reviewBody\">Review’s body</label><textarea name=\"\" class=\"form-control reviewBody\" placeholder=\"Type your review’s body here..\" data-id=\""+(0)+"\"></textarea></div></div>" +
+    "                   <div class=\"row mb-5\"><div class=\"col-5\"><label class=\"text-black font-weight-bold\" for=\"authorReview\">Author</label><input type=\"text\" name=\"\" class=\"form-control authorReview\" placeholder=\"Type author name here..\" value=\"\" data-id=\""+(0)+"\"></div>" +
+    "                   <div class=\"col-6\"><label class=\"text-black font-weight-bold\" for=\"publisher\">Publisher</label><input type=\"text\" name=\"\" class=\"form-control publisher\" placeholder=\"Type publisher here..\" value=\"\" data-id=\""+(0)+"\"></div>" +
+    "                   <div class=\"col-1\"><div class=\"d-flex justify-content-center mt-9\"><i class=\"bx bxs-x-circle bx-md text-darkgrey deleteReview\"></i></div></div></div>"
     );
     let row = parseInt(jQuery('#json-format').val().split('\n').length);
     jQuery('#json-format').attr('rows',row);
@@ -402,6 +422,7 @@ jQuery(document).on('click', '.deleteImage', function () {
         }
         jQuery('.deleteImage[data-id=' + main.image.length + ']').remove();
         jQuery('.image[data-id=' + main.image.length + ']').remove();
+        jQuery('.row[data-id=' + main.image.length + ']').remove();
         
         if (main.image.length == 1) {
             imageSingle = true;
@@ -410,6 +431,18 @@ jQuery(document).on('click', '.deleteImage', function () {
         }
     }
     sticky.update();
+});
+
+$(document).on('click', '.deleteIngredients', function () {
+    $('#ingredients > .row').remove();
+});
+
+$(document).on('click', '.deleteStep', function () {
+    $('#step > .row').remove();
+});
+
+$(document).on('click', '.deleteReview', function () {
+    $('#addReview > .row').remove();
 });
 
 jQuery('#copy').click(function () {
